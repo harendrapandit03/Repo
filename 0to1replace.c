4de5b4a14b829cff52c5a322e78dc0c86e55f058
harendrapandit03 <6206.harendra@gmail.com>
2025-10-12 18:12:52 +0530

    Add files via upload
    Suggestions for improving this code are welcome. If you have a better implementation or alternative method, please share your insights

---
+++ 0to1replace.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int main()
+{
+    int num,b,c,f,g;
+    printf("enter the number you want to check");
+    scanf("%d", &num);
+    f=printf("the number you have entered is:%d\n",num);
+    f-=32;
+    g=0;
+    for(int i=1;i<=f;i++){
+        b=num%10;
+        g=g+b*pow(10,f-i);
+        num=num/10;
+
+    }
+    for(int i=1;i<=f;i++){
+        if(g%10==0){
+            printf("1");
+            g=g/10;
+
+        }else{c=g%10;
+              printf("%d",c);
+              g=g/10;
+        }
+    }
+
+    return 0;
+
+}
+
